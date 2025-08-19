d8fdcad7c3c66893aa0d99ca4720edea2ab5ffc6
leon528hz <leon528hz@icloud.com>
2025-08-19 19:36:52 +0100

    Create supabaseClient.ts

---
+++ app/dashboard/app/dashboard/sections/signout/lib/supabaseClient.ts
@@ -0,0 +1,10 @@
+"use client";
+import { createClient as createBrowserClient } from "@supabase/supabase-js";
+
+export function createClient() {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    { auth: { persistSession: true, autoRefreshToken: true } }
+  );
+}
